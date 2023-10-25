8b7e54b1560dd0e5f4e0912fcca035b02a903ac4
kareem Elsayed <58936937+kareemlashin@users.noreply.github.com>
2023-10-25 14:16:43 +0300

    c

--- src/app/core/interceptor/interceptor.ts
+++
@@ -1,6 +0,0 @@
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-
-export const Interceptors = [
-    //   { provide: HTTP_INTERCEPTORS, useClass:  classInterceptor, multi: true },
-
-];
--- src/app/core/validators/confirm-validators.ts
+++
@@ -1,8 +0,0 @@
-import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
-
-export const passwordMatchingValidatior: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-    const password = control.get('password');
-    const confirmPassword = control.get('password_confirmation');
-  
-    return password?.value === confirmPassword?.value ? null : { notmatched: true };
-  };
